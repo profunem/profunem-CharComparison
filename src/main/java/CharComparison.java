45a1b6b8676b4332dbfd741a786c4fdca87ba7b3
Kung Lo <professionally_unemployed@proton.me>
2023-04-12 23:53:54 +0000

    Passed

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,17 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        int loopLen = Math.min(a.length, b.length);
+
+        for(int i = 0; i < loopLen; i++)
+        {
+            if(Character.toLowerCase(a[i]) == Character.toLowerCase(b[i]))
+            {
+                continue;
+            }
+            return a[i] < b[i] ? -1 : 1;
+        }
+
+        return a.length == b.length ? 0 : a.length < b.length ? -1 : 1;
     }
 }
